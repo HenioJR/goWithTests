2a5bb8ac72cef040ad6c8b6d728d670874e6cb32
Henio Bez <henio.bez@neoway.com.br>
2019-09-13 10:13:24 -0300

    package struct: create an interface to parametric polymorphism; implement table driven tests to Shape.Area()

--- struct/struct_test.go
+++ struct/struct_test.go
@@ -1,6 +1,8 @@
 package structs
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestPerimeter(t *testing.T) {
 	rectangle := Rectangle{10.0, 10.0}
@@ -14,12 +16,23 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{12.0, 6.0}
 
-	got := Area(rectangle)
-	want := 72.0
+	//table driven tests
+	//are useful when you want to build a list of test cases that can be tested in the same manner
+	areaTests := []struct {
+		name  string
+		shape Shape
+		want  float64
+	}{
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, want: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, want: 314.1592653589793},
+		{"Triangle", Triangle{12, 6}, 36.0},
+	}
 
-	if got != want {
-		t.Errorf("got '%.2f' want '%.2f'", got, want)
+	for _, tdt := range areaTests {
+		got := tdt.shape.Area()
+		if got != tdt.want {
+			t.Errorf("%#v got '%v' want '%v'", tdt, got, tdt.want)
+		}
 	}
 }
