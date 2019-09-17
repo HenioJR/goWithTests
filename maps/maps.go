66d00998ff897377fd9a9be7ebebcce96f5fd08a
Henio Bez <henio.bez@neoway.com.br>
2019-09-17 16:31:06 -0300

    package maps: new methods Add, Update and Delete

--- maps/maps.go
+++ maps/maps.go
@@ -2,7 +2,10 @@ package maps
 
 import "errors"
 
-var ErrUnknownWord = errors.New("Sorry, unknown word")
+var (
+	ErrUnknownWord  = errors.New("Sorry, unknown word")
+	ErrRepeatedWord = errors.New("Sorry, repeated word")
+)
 
 type Dictionary map[string]string
 
@@ -13,6 +16,27 @@ func (d Dictionary) Search(key string) (string, error) {
 	return "", ErrUnknownWord
 }
 
-func (d Dictionary) Add(key, value string) {
+//Map types are reference types, like pointers or slices
+//because of this, Dictionary does not a pointer
+func (d Dictionary) Add(key, value string) error {
+	_, err := d.Search(key)
+
+	switch err {
+	case ErrUnknownWord:
+		d[key] = value
+	case nil:
+		return ErrRepeatedWord
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Update(key, value string) {
 	d[key] = value
+}
+
+func (d Dictionary) Delete(key string) {
+	delete(d, key)
 }
--- maps/maps_test.go
+++ maps/maps_test.go
@@ -22,14 +22,62 @@ func TestSearch(t *testing.T) {
 }
 
 func TestAdd(t *testing.T) {
-	dictionary := Dictionary{}
-	dictionary.Add("new word", "nova palavra")
+	t.Run("add new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		errAdd := dictionary.Add("new word", "nova palavra")
 
-	got, err := dictionary.Search("new word")
-	want := "nova palavra"
+		got, err := dictionary.Search("new word")
+		want := "nova palavra"
+
+		assertStrings(t, got, want)
+		assertNoError(t, err)
+		assertNoError(t, errAdd)
+	})
+	t.Run("add repeated word", func(t *testing.T) {
+		dictionary := Dictionary{"repeated word": "palavra repetida"}
+		errAdd := dictionary.Add("repeated word", "nova traducao")
+
+		got, err := dictionary.Search("repeated word")
+		want := "palavra repetida"
+
+		assertStrings(t, got, want)
+		assertNoError(t, err)
+		assertError(t, errAdd, ErrRepeatedWord.Error())
+	})
+}
+
+func TestUpdate(t *testing.T) {
+	t.Run("known word", func(t *testing.T) {
+		dictionary := Dictionary{"apple": "banana"}
+		dictionary.Update("apple", "maca")
+
+		got, err := dictionary.Search("apple")
+		want := "maca"
+
+		assertStrings(t, got, want)
+		assertNoError(t, err)
+	})
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{"apple": "banana"}
+		dictionary.Update("car", "carro")
+
+		got, err := dictionary.Search("car")
+		want := "carro"
+
+		assertStrings(t, got, want)
+		assertNoError(t, err)
+	})
+}
+
+func TestDelete(t *testing.T) {
+	dictionary := Dictionary{"car": "carro"}
+	dictionary.Delete("car")
+
+	got, err := dictionary.Search("car")
+	want := ""
 
 	assertStrings(t, got, want)
-	assertNoError(t, err)
+	assertError(t, err, ErrUnknownWord.Error())
 }
 
 func assertStrings(t *testing.T, got, want string) {
