a1d34c71c618abdc57e9058f32a3d5203420ac57
Henio Bez <henio.bez@neoway.com.br>
2019-09-17 15:59:13 -0300

    package maps: add tests and refactory search method

--- maps/maps.go
+++ maps/maps.go
@@ -1,7 +1,14 @@
 package maps
 
+import "errors"
+
+var ErrUnknownWord = errors.New("Sorry, unknown word")
+
 type Dictionary map[string]string
 
-func (d Dictionary) Search(key string) string {
-	return d[key]
+func (d Dictionary) Search(key string) (string, error) {
+	if d[key] != "" {
+		return d[key], nil
+	}
+	return "", ErrUnknownWord
 }
--- maps/maps_test.go
+++ maps/maps_test.go
@@ -5,10 +5,45 @@ import "testing"
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"book": "livro"}
 
-	got := dictionary.Search("book")
-	want := "livro"
+	t.Run("known word", func(t *testing.T) {
+		got, err := dictionary.Search("book")
+		want := "livro"
+		assertStrings(t, got, want)
+		assertNoError(t, err)
+	})
 
+	t.Run("unknown word", func(t *testing.T) {
+		got, err := dictionary.Search("strange word")
+		want := ""
+		assertStrings(t, got, want)
+		assertError(t, err, ErrUnknownWord.Error())
+
+	})
+}
+
+func assertStrings(t *testing.T, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got '%s' want '%s'", got, want)
+	}
+}
+
+func assertError(t *testing.T, got error, want string) {
+	t.Helper()
+
+	if got == nil {
+		t.Error("wanted an error bug didn't get one")
+	}
+
+	if got.Error() != want {
+		t.Errorf("got '%s' want '%s'", got, want)
+	}
+}
+
+func assertNoError(t *testing.T, got error) {
+	t.Helper()
+
+	if got != nil {
+		t.Error("got an error but didn't want one")
 	}
 }
