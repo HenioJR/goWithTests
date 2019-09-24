79bbfbc2420757b7053587944824d579c8969352
Henio Bez <henio.bez@neoway.com.br>
2019-09-24 08:31:17 -0300

    create a server on injection

--- injection/intection.go
+++ injection/intection.go
@@ -3,8 +3,21 @@ package injection
 import (
 	"fmt"
 	"io"
+	"net/http"
 )
 
 func Salute(w io.Writer, name string) {
 	fmt.Fprintf(w, "Hello, %s", name)
+}
+
+func HandlerSalute(rw http.ResponseWriter, r *http.Request) {
+	Salute(rw, "world")
+}
+
+func main() {
+	err := http.ListenAndServe(":5000", http.HandlerFunc(HandlerSalute))
+
+	if err != nil {
+		fmt.Println(err)
+	}
 }
