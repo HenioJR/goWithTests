ed8315b13d0c5bc22e40db0e38e8293d9cc507e9
Henio Bez <henio.bez@neoway.com.br>
2019-10-07 11:24:33 -0300

    improve select func to use constructor select

--- select/select.go
+++ select/select.go
@@ -2,22 +2,24 @@ package racer
 
 import (
 	"net/http"
-	"time"
 )
 
 func Racer(url1, url2 string) (fastURL string) {
-	durationURL1 := measureResponseTime(url1)
-	durationURL2 := measureResponseTime(url2)
-
-	if durationURL1 < durationURL2 {
+	//constructor select helps us synchronise processes easily and clearly
+	//select wait on multiple channels. The first one to send a value "wins"
+	select {
+	case <-ping(url1):
 		return url1
+	case <-ping(url2):
+		return url2
 	}
-
-	return url2
 }
 
-func measureResponseTime(url string) time.Duration {
-	start := time.Now()
-	http.Get(url)
-	return time.Since(start)
+func ping(url string) chan bool {
+	ch := make(chan bool)
+	go func() {
+		http.Get(url)
+		ch <- true
+	}()
+	return ch
 }
